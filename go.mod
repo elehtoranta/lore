81ff004d0397710877598dc0d7cc2f8994854cff
Erkka Lehtoranta <erkka.lehtoranta@gmail.com>
2025-02-13 06:21:09 +0200

    Fetch initial game state

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module erkka.dev/lore
 
 go 1.23.3
+
+require (
+	github.com/google/go-cmp v0.6.0
+	github.com/joho/godotenv v1.5.1
+)
--- main.go
+++ main.go
@@ -1,13 +1,68 @@
 package main
 
 import (
-	"fmt"
-	"io"
-	"log"
-	"net/http"
+    "encoding/json"
+    "fmt"
+    "io"
+    "log"
+    "net/http"
+    "os"
+
+    "github.com/joho/godotenv"
 )
 
-const URL = "https://koodipahkina.monad.fi/api"
+// Game state populated by API responses.
+type GameState struct {
+    GameId string `json:"gameId"`
+    Status Status `json:"status"`
+}
+
+type Status struct {
+    Card uint8 `json:"card"`
+    Money uint8 `json:"money"`
+    Players []Player `json:"players"`
+    CardsLeft uint8 `json:"cardsLeft"`
+    Finished bool `json:"finished"`
+}
+
+type Player struct {
+    Name string `json:"name"`
+    Money uint16 `json:"money"`
+    Cards [][]uint16 `json:"cards"`
+}
+
+const (
+    URL = "https://koodipahkina.monad.fi/api"
+)
+
+// Requests a new game and returns its ID on success, and writes error on failure.
+func initGame(url string, gs *GameState) (string, error) {
+    var buf io.Reader
+
+    client := http.DefaultClient
+
+    req, err := http.NewRequest("POST", url, buf)
+    if err != nil {
+        log.Fatal(err)
+    }
+    key, found := os.LookupEnv("LORE_API_KEY")
+    if !found {
+        log.Fatal(err)
+    }
+    req.Header.Add("Authorization", "Bearer "+key)
+
+    res, err := client.Do(req)
+    if err != nil {
+        return "", err
+    }
+    defer res.Body.Close()
+
+    err = json.NewDecoder(res.Body).Decode(&gs)
+    if err != nil {
+        return "", err
+    }
+    return gs.GameId, nil
+}
 
 // ping the server for a happy and stress free start to the project
 func ping() string {
@@ -28,5 +83,16 @@ func ping() string {
 }
 
 func main() {
-    fmt.Println("Ping:", ping())
+    // This will not override any keys, so an API key loaded to the test process via `env` command
+    // will be used instead of anything in .env. Github CI tests should be using a mock key. Should. :D
+    err := godotenv.Load(".env")
+    if err != nil {
+        log.Fatal("Error loading .env file")
+    }
+    var gs GameState
+    gameId, err := initGame(URL+"/game", &gs)
+    if err != nil || gameId == "" {
+        log.Fatal("Game initialization returned error.", err)
+    }
+    // fmt.Println("Ping:", ping())
 }
